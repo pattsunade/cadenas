29fc127675781ae3a106fb3ec8e2cdba52bc30d6
pattsunade <patricia.stylesh@gmail.com>
2021-07-21 18:43:36 -0400

    cadenas

---
+++ cadenas2/src/main/java/com/patriciadelgado/cadenas2/Cadenas2Application.java
@@ -0,0 +1,13 @@
+package com.patriciadelgado.cadenas2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cadenas2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cadenas2Application.class, args);
+	}
+
+}
